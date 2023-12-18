a5cac57a0c74b0b40731413b4b7e49cf3c3fffd1
Rudra Bhikadiya <irudradev@gmail.com>
2023-12-18 19:51:31 +0530

    layout

---
+++ pages/workflow.js
@@ -0,0 +1,81 @@
+import classNames from 'classnames'
+import React, { useState } from 'react'
+import { VscFile } from 'react-icons/vsc'
+
+const Workflow = () => {
+  const localCsvs = [
+    {
+      name: 'csv1',
+      path: 'https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv',
+    },
+    {
+      name: 'csv2',
+      path: 'https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv',
+    },
+    {
+      name: 'csv3',
+      path: 'https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv',
+    },
+  ]
+  const [sidebar, setSidebar] = useState(false)
+  return (
+    <main>
+      <div>
+        <nav className='fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700'>
+          <div className='px-3 py-3 lg:px-5 lg:pl-3'>
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center justify-start rtl:justify-end'>
+                <button
+                  className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+                  onClick={() => setSidebar(!sidebar)}
+                >
+                  <span className='sr-only'>Open sidebar</span>
+                  <svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+                    <path
+                      clipRule='evenodd'
+                      fillRule='evenodd'
+                      d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z'
+                    />
+                  </svg>
+                </button>
+                <a href='/' className='flex ms-2 md:me-24'>
+                  <span className='self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white'>Workflow</span>
+                </a>
+              </div>
+              <div className='flex items-center'></div>
+            </div>
+          </div>
+        </nav>
+        <aside
+          className={classNames(
+            'fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700',
+            {
+              '-translate-x-full': !sidebar,
+            }
+          )}
+        >
+          <div className='h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800'>
+            <ul className='space-y-2 font-medium'>
+              {localCsvs.map((csv) => (
+                <li>
+                  <a href='#' className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'>
+                    <VscFile />
+
+                    <span className='ms-3'>{csv.name}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </aside>
+        <div className='p-4 sm:ml-64'>
+          <div className='p-2 mt-14'>
+            <h1 className='text-red-700'>Hello</h1>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Workflow
